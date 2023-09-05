7367ca471cd4660e3102686e173031746932a61a
Anthony <andeng2@berkeley.edu>
2023-09-05 01:23:05 -0700

    text change

--- src/components/qualifications/qualifications.jsx
+++ src/components/qualifications/qualifications.jsx
@@ -6,14 +6,6 @@ const Qualifications = () => {
       <span className="quali-text">My Qualifications</span>
       <div className="qualifications-container">
         <div className="qualification">
-          <div className="content html"></div>
-          <h1>html</h1>
-        </div>
-        <div className="qualification">
-          <div className="content css"></div>
-          <h1>css</h1>
-        </div>
-        <div className="qualification">
           <div className="content javascript"></div>
           <h1>javascript</h1>
         </div>
@@ -22,16 +14,24 @@ const Qualifications = () => {
           <h1>python</h1>
         </div>
         <div className="qualification">
-          <div className="content react"></div>
-          <h1>react</h1>
-        </div>
-        <div className="qualification">
           <div className="content c"></div>
           <h1>c</h1>
         </div>
         <div className="qualification">
           <div className="content sql"></div>
           <h1>sql</h1>
+        </div>
+        <div className="qualification">
+          <div className="content html"></div>
+          <h1>html</h1>
+        </div>
+        <div className="qualification">
+          <div className="content css"></div>
+          <h1>css</h1>
+        </div>
+        <div className="qualification">
+          <div className="content react"></div>
+          <h1>react</h1>
         </div>
         <div className="qualification">
           <div className="content node"></div>
